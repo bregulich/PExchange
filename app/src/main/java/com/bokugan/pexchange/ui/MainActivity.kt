325a12d91396cf96a68079d6fc623a11b2f3f706
Bogdan Regulich <bogdanregulich@live.com>
2020-07-08 16:47:37 +0300

    Initial commit

---
+++ app/src/main/java/com/bokugan/pexchange/ui/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.bokugan.pexchange.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+import com.bokugan.pexchange.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
