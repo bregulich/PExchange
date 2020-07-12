3c84f50f395d05211c874cde4314e046d2ffdd3d
Bogdan Regulich <bogdanregulich@live.com>
2020-07-12 18:07:08 +0300

    UI

--- app/src/main/java/com/bokugan/pexchange/ui/currencyhistory/CurrencyHistoryActivity.kt
+++ app/src/main/java/com/bokugan/pexchange/ui/currencyhistory/CurrencyHistoryActivity.kt
@@ -1,13 +1,24 @@
 package com.bokugan.pexchange.ui.currencyhistory
 
+import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 
 import com.bokugan.pexchange.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_currency_history.*
 
 @AndroidEntryPoint
 class CurrencyHistoryActivity : AppCompatActivity(R.layout.activity_currency_history) {
 
     private val vm by viewModels<CurrencyHistoryViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbar)
+
+        recycler_view.apply {
+            setHasFixedSize(true)
+        }
+    }
 }
